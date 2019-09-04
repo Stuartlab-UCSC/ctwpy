0bee338199e76e5a306a2a9fbc269e19f3c86ace
Duncan <duncmc831@gmail.com>
2019-09-03 17:10:50 -0700

    add: filename constants

---
+++ ctwpy/filenames.py
@@ -0,0 +1,9 @@
+
+CLUSTERING = "clustering.pi"
+EXPRESSION = "exp.pi"
+XYS = "xys.pi"
+MARKER_TABLE = "markers.pi"
+CELL_TYPE_ANNOTATION = "celltype_annotate.pi"
+STATE = "state.json.gz"
+SPLIT_CLUSTER_DICT = "cluster_dict.pi"
+all_filenames = [CLUSTERING, EXPRESSION, XYS, MARKER_TABLE, STATE, CELL_TYPE_ANNOTATION]
