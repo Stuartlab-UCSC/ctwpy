439d7c243d631c9901e9e8482fe1cfdf83774272
Duncan <duncmc831@gmail.com>
2019-09-03 17:12:03 -0700

    add: setup.py with click functions

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ctwpy',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click', "scanpy>1", "scipy", "pandas", "beautifulsoup4", "requests"
+    ],
+    entry_points='''
+        [console_scripts]
+        ctw-scanpy-obs=ctwpy.cli:scanpy_obs
+        ctw-from-scanpy=ctwpy.cli:from_scanpy
+        ctw-upload=ctwpy.cli:upload_worksheet
+
+    ''',
+)
