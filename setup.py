bd32d94fc63a99e7aa931c9d9f127b6213f3e203
Duncan <duncmc831@gmail.com>
2019-09-17 08:02:03 -0700

    change to scanpy 1.3.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click', "scanpy>1", "scipy", "pandas", 
+        'Click', "scanpy==1.3.6", "scipy", "pandas",
         "beautifulsoup4", "requests", "requests_toolbelt", "urllib3==1.24.2"
     ],
     entry_points='''
@@ -14,6 +14,5 @@ setup(
         ctw-scanpy-obs=ctwpy.cli:scanpy_obs
         ctw-from-scanpy=ctwpy.cli:from_scanpy
         ctw-upload=ctwpy.cli:upload_worksheet
-
     ''',
 )
